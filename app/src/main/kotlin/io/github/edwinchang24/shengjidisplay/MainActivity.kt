5a664d8c7d428054af60e88ff46a8eef57907a20
Edwin Chang <edwinchang2024@gmail.com>
2024-04-27 10:55:49 -0700

    fix nav bar color on API 29+

--- app/src/main/kotlin/io/github/edwinchang24/shengjidisplay/MainActivity.kt
+++ app/src/main/kotlin/io/github/edwinchang24/shengjidisplay/MainActivity.kt
@@ -1,10 +1,13 @@
 package io.github.edwinchang24.shengjidisplay
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
@@ -26,7 +29,10 @@ class MainActivity : ComponentActivity() {
         viewModel.load(this)
         setContent {
             ShengJiDisplayTheme {
-                Surface(color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    color = MaterialTheme.colorScheme.background,
+                    modifier = Modifier.fillMaxSize().windowInsetsPadding(WindowInsets(0))
+                ) {
                     DestinationsNavHost(
                         navGraph = NavGraphs.main,
                         dependenciesContainerBuilder = { dependency(viewModel) },
