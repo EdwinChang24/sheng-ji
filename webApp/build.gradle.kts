4e7ad6d6debf03b195545bb2da6b860923856564
Edwin Chang <edwinchang2024@gmail.com>
2024-06-25 17:23:16 -0700

    add data storage to web

--- webApp/build.gradle.kts
+++ webApp/build.gradle.kts
@@ -39,6 +39,7 @@ kotlin {
                 implementation(compose.foundation)
                 implementation(compose.material3)
                 implementation(compose.components.resources)
+                implementation(libs.kotlinx.serialization)
             }
         }
     }
--- webApp/src/wasmJsMain/kotlin/Main.kt
+++ webApp/src/wasmJsMain/kotlin/Main.kt
@@ -4,14 +4,32 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
 import io.github.edwinchang24.shengjidisplay.App
 import io.github.edwinchang24.shengjidisplay.model.AppState
+import kotlinx.browser.localStorage
 import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.jetbrains.compose.resources.configureWebResources
 
 @OptIn(ExperimentalComposeUiApi::class, DelicateCoroutinesApi::class)
 fun main() {
     configureWebResources { resourcePathMapping { path -> "./$path" } }
-    val appState = MutableStateFlow(AppState())
+    val storageKey = "state"
+    val appState =
+        MutableStateFlow(
+            localStorage.getItem(storageKey)?.let { data -> Json.decodeFromString(data) }
+                ?: AppState()
+        )
+    GlobalScope.launch {
+        appState.collectLatest { state ->
+            delay(100)
+            localStorage.setItem(storageKey, Json.encodeToString(state))
+        }
+    }
     CanvasBasedWindow(canvasElementId = "app") {
         val state by appState.collectAsState()
         App(state, { appState.value = it })
