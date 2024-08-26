02293d2fe14c23d6744eb3b0f642f914ec491bc6
Edwin Chang <edwinchang2024@gmail.com>
2024-08-25 22:12:16 -0700

    add links worker

---
+++ display-links/src/index.ts
@@ -0,0 +1,20 @@
+export default {
+    async fetch(request, _env, _ctx): Promise<Response> {
+        const url = new URL(request.url);
+        if (url.pathname === "/.well-known/asset-links.json") {
+            return Response.json([
+                {
+                    relation: ["delegate_permission/common.handle_all_urls"],
+                    target: {
+                        namespace: "android_app",
+                        package_name: "dev.edwinchang.shengjidisplay",
+                        sha256_cert_fingerprints: [
+                            // TODO
+                        ],
+                    },
+                },
+            ]);
+        }
+        return Response.redirect("https://example.com", 303); // TODO
+    },
+} satisfies ExportedHandler<Env>;
