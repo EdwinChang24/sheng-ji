60603ba2cfee140c3f5cc9eaeb545aef35b72124
Edwin Chang <edwinchang2024@gmail.com>
2024-08-25 22:15:18 -0700

    fix file name

--- display-links/src/index.ts
+++ display-links/src/index.ts
@@ -1,7 +1,7 @@
 export default {
     async fetch(request, _env, _ctx): Promise<Response> {
         const url = new URL(request.url);
-        if (url.pathname === "/.well-known/asset-links.json") {
+        if (url.pathname === "/.well-known/assetlinks.json") {
             return Response.json([
                 {
                     relation: ["delegate_permission/common.handle_all_urls"],
