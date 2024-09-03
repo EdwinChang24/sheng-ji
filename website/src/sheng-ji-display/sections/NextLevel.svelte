0433886d626c5212438b098d2d4e102d9e01a48f
Edwin Chang <edwinchang2024@gmail.com>
2024-09-02 17:00:25 -0700

    fix line break

--- website/src/sheng-ji-display/sections/NextLevel.svelte
+++ website/src/sheng-ji-display/sections/NextLevel.svelte
@@ -2,7 +2,12 @@
     import Buttons from "../components/Buttons.svelte";
 </script>
 
-<div class="min-h-[50dvh] bg-slate-900 flex flex-col space-y-16 justify-center items-center py-16 px-4">
-    <p class="text-6xl font-bold text-center">Next level.</p>
+<div
+    class="min-h-[50dvh] bg-slate-900 flex flex-col space-y-16 justify-center items-center py-16 px-4"
+>
+    <div class="flex max-[400px]:flex-col flex-row text-6xl font-bold">
+        <p class="">Next&nbsp;</p>
+        <p>level.</p>
+    </div>
     <Buttons />
 </div>
