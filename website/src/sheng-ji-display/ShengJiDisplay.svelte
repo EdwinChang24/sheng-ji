39d0c3f874ad05737a86bb29ccb908da95b82495
Edwin Chang <edwinchang2024@gmail.com>
2024-09-03 15:56:27 -0700

    add more site meta

---
+++ website/src/sheng-ji-display/ShengJiDisplay.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import WorksOn from "./sections/WorksOn.svelte";
+    import Foss from "./sections/FOSS.svelte";
+    import LevelUp from "./sections/LevelUp.svelte";
+    import NextLevel from "./sections/NextLevel.svelte";
+    import PossibleTrumps from "./sections/PossibleTrumps.svelte";
+    import Teammates from "./sections/Teammates.svelte";
+    import TrumpCalls from "./sections/TrumpCalls.svelte";
+    
+    let scroll = 0;
+    let height: number;
+</script>
+
+<svelte:window bind:scrollY={scroll} bind:innerHeight={height} />
+
+<LevelUp />
+<TrumpCalls />
+<PossibleTrumps />
+<Teammates />
+<WorksOn />
+<Foss />
+<NextLevel />
